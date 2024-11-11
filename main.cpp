2bbdc33352295c67395a7f94a15471fb383c5d37
Amir Hajipoor <amir.hjr@outlook.com>
2024-11-11 12:23:16 +0330

    init project

---
+++ main.cpp
@@ -0,0 +1,108 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+struct Student
+{
+    long id;
+    string firstName;
+    string lastName;
+    string phoneNumber;
+};
+
+vector<Student>list;
+
+void backToMenu();
+
+void showStudent()
+{
+    for (const auto &st : list)
+    {
+        cout << "id: " << st.id << "\t";
+        cout << "first name: " << st.firstName << "\t";
+        cout << "last name: " << st.lastName << "\t";
+        cout << "phone number: " << st.phoneNumber << "\t";
+        cout << endl;
+
+        backToMenu();
+    }
+}
+
+void AddStudent()
+{
+    long id;
+    string firstName, lastName, phoneNumber;
+
+    cout << "Enter id: ";
+    cin >> id;
+    cout << endl << "Enter first name: ";
+    cin >> firstName;
+    cout << endl << "Enter last name: ";
+    cin >> lastName;
+    cout << endl << "Enter phone number: ";
+    cin >> phoneNumber;
+
+    Student st;
+    st.id = id;
+    st.firstName = firstName;
+    st.lastName = lastName;
+    st.phoneNumber = phoneNumber;
+
+    list.push_back(st);
+    backToMenu();
+}
+
+void showMenu()
+{
+    int q = 0;
+
+    do {
+        cout << "Welcome, what do you want to do?" << endl;
+        cout << "1) Show students" << endl;
+        cout << "2) Add student" << endl;
+        cin >> q;
+
+    } while (q < 1 || q > 3);
+
+    switch (q)
+    {
+    case 1:
+        showStudent();
+        break;
+    case 2:
+        AddStudent();
+        break;
+    case 3:
+        break;
+    default:
+        cout << "404 not found!";
+    }
+
+}
+
+void backToMenu()
+{
+    cout << endl << "success!" << endl;
+    char c = 'n';
+
+    do {
+        cout << "back to menu? ";
+        c = getchar();
+    } while (c != 'y' && c != 'n');
+
+    if (c == 'y')
+        showMenu();
+}
+
+int main()
+{
+    showMenu();
+
+    // for (auto it = list.rbegin(); it != list.end(); it++)
+    // {
+    //     if (it->id == 1) list.erase(it);
+    //     it--;
+    // }
+}
